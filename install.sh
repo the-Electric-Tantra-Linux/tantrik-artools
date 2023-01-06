b12bf87d662adb9bd8cb6922bb8582c00b110b28
Thomashighbaugh <thighbaugh@zoho.com>
2023-01-06 06:19:08 -0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Not that it is necessary and maybe its not a PKGBUILD (yet) but just to ease installation of this minor modification here is an install.sh
+
+sudo make 
+
+sudo make install
